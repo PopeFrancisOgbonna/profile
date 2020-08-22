568e67471d61c8cda18d96a235581fbea136a315
PopeFrancisOgbonna <francisogbonna24@gmail.com>
2020-08-22 14:17:17 +0100

    added media query to iframe

--- src/js/index.js
+++ src/js/index.js
@@ -23,16 +23,21 @@ toggle.addEventListener('click', (e) =>{
     }
 })
 //Google form handler
+const social = document.querySelector('.social-icon');
 const hireMe = document.querySelector('#hireMeBtn');
 const frame = document.querySelector('.iframeDiv');
 const closeHire = document.querySelector('#hireClose');
 hireMe.addEventListener('click', (e) =>{
     e.preventDefault();
     frame.classList.remove('hide');
+    if(screen.width < 450){
+        social.classList.add('hide');
+    }
 });
 closeHire.addEventListener('click', (e) =>{
     e.preventDefault();
     frame.classList.add('hide');
+    social.classList.remove('hide');
 })
 //Modal section but was Removed in favor of google form
 const email=document.querySelector('#email');
@@ -43,24 +48,24 @@ const send=document.querySelector('.send');
 const parttern=/^[a-zA-Z]+$/;
 const mail=/\S+\@\S+\.\S+/;
 
-send.addEventListener('click',(e)=>{
-    e.preventDefault();
-    if( name.value.match(parttern)&& email.value.match(mail)){
-        if(message.value!=''){
-           setTimeout(() => {
-              alert('Message Sent'); 
-              email.value='';
-              name.value='';
-              message.value='';
-            $('#hire-modal').modal('hide');
-           }, 2000);
-        }else{
-            alert('You can\'t Send empty message');
-        }
+// send.addEventListener('click',(e)=>{
+//     e.preventDefault();
+//     if( name.value.match(parttern)&& email.value.match(mail)){
+//         if(message.value!=''){
+//            setTimeout(() => {
+//               alert('Message Sent'); 
+//               email.value='';
+//               name.value='';
+//               message.value='';
+//             $('#hire-modal').modal('hide');
+//            }, 2000);
+//         }else{
+//             alert('You can\'t Send empty message');
+//         }
         
-    }
-    else{
-        alert('Incorrect input!');
-    }
-});
+//     }
+//     else{
+//         alert('Incorrect input!');
+//     }
+// });
 //
